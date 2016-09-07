643c2011593c2a560a5b43ff3bfd049625f75e64
Kuan <huangkuanlin@gmail.com>
2016-09-07 15:16:02 -0500

    Added preprocessPhosphoDatabase.R to compile phospho databases used in this study

---
+++ preprocess_files/preprocessPhosphoDatabase.R
@@ -0,0 +1,10 @@
+##### preprocessPhosphoDatabase.R #####
+# Kuan-lin Huang @ WashU 2016 Sep
+# preprocess phosphodatabases
+
+library(limma)
+baseD = "/Users/khuang/Box Sync/PhD/proteogenomics/CPTAC_pan3Cancer/pan3can_shared_data/"
+
+setwd("/Users/khuang/Box\ Sync/PhD/proteogenomics/CPTAC_pan3Cancer/pan3can_analysis/preprocess_files/")
+source("/Users/khuang/bin/LIB_exp.R")
+
