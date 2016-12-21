fea942a3db3cd83e87ac4ab7bf7df7817ad762d6
Kuan <huangkuanlin@gmail.com>
2016-12-20 22:19:19 -0600

    changes to streamline code and change plotting aesthetics

--- phospho_network/regression_kinase_match_sub.R
+++ phospho_network/regression_kinase_match_sub.R
@@ -9,9 +9,17 @@ library(reshape)
 library(grid)
 require(plyr)
 
+
+# for working on Kuan's mac
+baseD = "/Users/khuang/Box\ Sync/PhD/proteogenomics/CPTAC_pan3Cancer/"
+setwd(paste(baseD,"pan3can_analysis/phospho_network",sep=""))
+
+source("../pan3can_aes.R") # aes for general purposes; it should be one directory out of the working directory
+
 # input regardless to cancer type-------------------------------------------------------------------
 ### read in the kinase/substrate table/ phosphorylation data ### 
 K_S_f ="~/Box Sync/Phospho_databases/PhosphositePlus/data/Kinase_Substrate_Dataset_human_final_hugoified.txt"
+K_S_f = paste(baseD,"pan3can_shared_data/Phospho_databases/PhosphositePlus/data/Kinase_Substrate_Dataset_human_final_hugoified.txt",sep="")
 k_s_table = read.delim(K_S_f)
 
 #function for normalize the variables for regression
@@ -31,23 +39,30 @@ for (cancer in c("BRCA","OV")) {
     # BRCA
     cancer = "BRCA"
     BRCA_pro_f = "~/Box Sync/BRCA/BRCA_PRO_formatted_normalized.txt"
+    BRCA_pro_f = paste(baseD,"pan3can_shared_data/BRCA/BRCA_PRO_formatted_normalized_max10NA.txt",sep="")
     pro_data <- read.delim(BRCA_pro_f)
     BRCA_pho_f = "~/Box Sync/BRCA/TCGA_Breast_BI_Phosphoproteome.phosphosite.itraq_abbrev_normlized.tsv"
+    BRCA_pho_f = paste(baseD,"pan3can_shared_data/BRCA/TCGA_Breast_BI_Phosphoproteome.phosphosite.itraq_abbrev_normlized.tsv",sep="")
     pho_data = read.delim(BRCA_pho_f)
     ## read in grouped phosphorylation data!
     BRCA_pho_g = "~/Box Sync/BRCA/BRCA_PHO_by_PRO_formatted_normalized.txt"
+    BRCA_pho_g = paste(baseD,"pan3can_shared_data/BRCA/BRCA_PHO_by_PRO_formatted_normalized.txt",sep="")
     pho_gdata = read.delim(BRCA_pho_g)
     colx <- 78 # the column of protein name
   } else {
     #OV
     cancer = "OV"
     OV_pho_f = "~/Box Sync/OV/TCGA_Ovarian_PNNL_Phosphoproteome.phosphosite.itraq_abbrev_normlized.tsv"
+    OV_pho_f = paste(baseD,"pan3can_shared_data/OV/TCGA_Ovarian_PNNL_Phosphoproteome.phosphosite.itraq_abbrev_normalized_max10NA.tsv",sep="")
     pho_data = read.delim(OV_pho_f)
     OV_pho_g = "~/Box Sync/OV/OV_PNNL_PHO_by_PRO_formatted_normalized.txt"
+    OV_pho_g = paste(baseD,"pan3can_shared_data/OV/OV_PNNL_PHO_by_PRO_formatted_normalized_max10NA.txt",sep="")
     pho_gdata = read.delim(OV_pho_g)
     OV_pro_f = "~/Box Sync/OV/OV_merged_PRO_noOverlap_formatted_normalized.txt"
-    pro_data_merged <- read.delim(OV_pro_f)
-    pro_data <- pro_data_merged[,colnames(pho_data)]
+    OV_pro_f = paste(baseD,"pan3can_shared_data/OV/OV_PNNL_PRO_formatted_normalized_max10NA.txt",sep="")
+    pro_data <- read.delim(OV_pro_f)
+    #pro_data_merged <- read.delim(OV_pro_f)
+    #pro_data <- pro_data_merged[,colnames(pho_data)]
     colx <- 1 # the column of protein name
   }
   
@@ -285,6 +300,7 @@ for (cancer in c("BRCA","OV")) {
 # make the table for bubble chart -------------------------------------------------------
 ## combine table from BRCA and OV
 table_2can <- rbind(table_BRCA,table_OV)
+name = c("pro_kin","pro_sub","pho_kin")
 
 ## adjust p-values to FDR
 for (mod in c("pho_sub~pro_kin", "pho_sub~pro_kin+pro_sub", "pho_sub~pro_kin+pro_sub+pho_kin","pho_sub~pho_kin")) {
@@ -297,6 +313,11 @@ for (mod in c("pho_sub~pro_kin", "pho_sub~pro_kin+pro_sub", "pho_sub~pro_kin+pro
     }
   }
 }
+### I can't run OV for some reason; could you trouble shoot this?
+
+## write out tables
+tn = paste(td,"kinase_substrate_regression.txt", sep="_")
+write.table(table_2can, file=tn, quote=F, sep = '\t', col.names=NA)
 
 ## only plot result for model1
 table_sig <- table_2can[table_2can$model=="pho_sub~pro_kin" & table_2can$FDR_pro_kin <= sig,]
@@ -304,7 +325,7 @@ table_sig_self <- table_sig[table_sig$self,]
 table_sig_other <- table_sig[!table_sig$self,]
 
 # choose the parameters for bubble chart ---------------------
-top <- 100 # choose top n rows for FDR_pro_kin for model1
+top <- 50 # choose top n rows for FDR_pro_kin for model1
 c <- "BRCA" #choose in which cancer extract the top n rows
 t0 <- table_sig_other  ## choose table_sig_other or table_sig_self
 
@@ -320,36 +341,23 @@ for(i in 1:top){
   rows <- c(rows,r)
 }
 table <- t0[rows,]
-
-## reshapte the table 
-temp <- c("KINASE","SUBSTRATE","SUB_MOD_RSD","size","model","P_pro_kin","P_pro_sub","P_pho_kin","pair","Cancer","self","order","pairs")
-dm1 <- melt(table[,c(temp,paste("FDR_",name,sep = ""))], id=temp)
-colnames(dm1) <- c(temp, "variable", "FDR")
-dm1$var <- vector(mode = "numeric", length = nrow(dm1))
-for (v in name) {
-  dm1$var[dm1$variable==paste("FDR_",v,sep = "")] <- v
-}
-
-
-dm2 <- melt(table[,c(temp,paste("coef_",name,sep = ""))], id=temp)
-colnames(dm2) <- c(temp, "variable2", "coefficients")
-dm2$var <- vector(mode = "numeric", length = nrow(dm2))
-for (v in name) {
-  dm2$var[dm2$variable2==paste("coef_",v,sep = "")] <- v
-}
-
-molten <- merge(dm1, dm2)
-molten <- molten[!is.na(molten$FDR),]
-molten <- molten[molten$var!="pro_sub" & molten$model!="pho_sub~pro_kinase+pro_sub+pho_kin_grouped",]
+#table_2can_brca_top = table_2can[table_2can$pair %in% table$pair,]
 
 # bubble chart plotting module ---------------------------------------------------------
+lim = max(max(table$coef_pro_kin),min(table$coef_pro_kin))
+
 ## actual plotting
 p = ggplot(table,aes(x=model, y=pair))# make this the original ethni
-p = p + facet_grid(.~Cancer, drop=T, space = "free_x",scales = "free_x")#, space = "free", scales = "free")
-p = p + geom_point(aes(colour=coef_pro_kin, size =-log10(FDR_pro_kin))) + theme_bw() #+ theme_nogrid()
+p = p + facet_grid(KINASE~Cancer, drop=T, space = "free_y",scales = "free_y")#, space = "free", scales = "free")
+p = p + geom_point(aes(fill=coef_pro_kin, size =-log10(FDR_pro_kin), color=ifelse(sig, "black",NA)),pch=21) 
+p = p + scale_fill_gradientn(name= "Coefficient", na.value=NA, colours=RdBu1024, limit=c(-lim,lim))
+p = p + scale_colour_manual(values=c("black",NA))
+p = p + theme_bw() #+ theme_nogrid()
 p = p + labs(x = "", y="kinase:substrate:SUB_MOD_RSD")
 p = p + theme(axis.title = element_text(size=10), axis.text.x = element_text(colour="black", size=6,angle=90, vjust=0.5), axis.text.y = element_text(colour="black", size=10))#element_text(colour="black", size=14))
 p
+fn = paste(pd, 'kinase_substrate_fdr0.05.pdf',sep ="_")
+ggsave(file=fn, height=10, width=5, useDingbats=FALSE)
 
 # volcano plotting module -------------------------------------------------
 remove_outliers <- function(x, na.rm = TRUE, ...) {
@@ -361,14 +369,27 @@ remove_outliers <- function(x, na.rm = TRUE, ...) {
   y
 }
 
-## choose the cancer, model and cis/trans
-c <- "OV"
-mode <- "pho_sub~pro_kin"
-cis <- FALSE
+# ## choose the cancer, model and cis/trans
+# c <- "OV"
+# mode <- "pho_sub~pro_kin"
+# cis <- FALSE
+# 
+# table <- table_2can[table_2can$Cancer==c & table_2can$model==mode & table_2can$self==cis,]
+# x1 <- remove_outliers(table$coef_pro_kin)
+# p <- qplot(coef_pro_kin, -log10(FDR_pro_kin), data = table[table$coef_pro_kin==x1,])
+# p
 
-table <- table_2can[table_2can$Cancer==c & table_2can$model==mode & table_2can$self==cis,]
-x1 <- remove_outliers(table$coef_pro_kin)
-p <- qplot(coef_pro_kin, -log10(FDR_pro_kin), data = table[table$coef_pro_kin==x1,])
+#table <- table_2can[table_2can$Cancer==c & table_2can$model==mode & table_2can$self==cis,]
+table_2can$coef_pro_kin_filtered = remove_outliers(table_2can$coef_pro_kin)
+table_2can_outlier_removed_m = table_2can[!is.na(table_2can$coef_pro_kin_filtered) & table_2can$model == mode,]
+
+p = ggplot(table_2can_outlier_removed_m,aes(x=coef_pro_kin, y=-log10(FDR_pro_kin)))
+p = p + facet_grid(self~Cancer,scales = "free_y")#, drop=T, space = "free_y",scales = "free_y")#, space = "free", scales = "free")
+p = p + geom_point(alpha=0.05)
+p = p + geom_text(aes(label= ifelse(-log10(FDR_pro_kin)>5, pair, NA)),size=2,alpha=0.5)
+p = p + theme_bw() #+ theme_nogrid()
+p = p + theme(axis.title = element_text(size=10), axis.text.x = element_text(colour="black", size=6,angle=90, vjust=0.5), axis.text.y = element_text(colour="black", size=10))#element_text(colour="black", size=14))
+p = p + labs(x = "Coefficient", y="-log10(FDR)")
 p
-
-
+fn = paste(pd, 'kinase_substrate_volcano.pdf',sep ="_")
+ggsave(file=fn, height=6, width=8, useDingbats=FALSE)
